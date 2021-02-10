e9869eaa66373b3ce9cbc02a4ad2627f80d0af7c
Kittipot <Kittipot321@gmail.com>
2021-02-10 13:20:14 +0700

    Fix 503

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn kittipotblog.wsgi
+web: gunicorn kittipotblog.wsgi --log-file -
--- myblog/settings.py
+++ myblog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -121,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 LOGIN_URL = '/login/'
+import django_heroku
+django_heroku.settings(locals())
