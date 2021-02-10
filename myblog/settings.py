4ae5d46900508b294e4923954fffee58d92a8b4d
Kittipot <Kittipot321@gmail.com>
2021-02-10 13:34:24 +0700

    2

--- myblog/settings.py
+++ myblog/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-LOGIN_URL = '/login/'
+LOGIN_URL = 'login'
 import django_heroku
 django_heroku.settings(locals())
